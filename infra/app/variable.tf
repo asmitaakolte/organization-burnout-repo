bd1dbc5e5bc5492ab8b8d762bb1e72d2897e0c06
Asmita Kolte <asmitaakolte@gmail.com>
2026-02-01 17:47:23 -0600

    Fix Terraform CI hang by removing interactive variable

--- infra/app/variable.tf
+++ infra/app/variable.tf
@@ -2,13 +2,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "github_org" {
-  description = "asmitaakolte"
-}
-
-variable "github_repo" {
-  description = "org-burnout-repo"
-}
 
 variable "project_name" {
   default = "mlops-burnout"
